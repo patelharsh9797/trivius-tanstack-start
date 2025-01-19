492089a27e08723bbb31eaaa4a26e2fbab0288e8
Lazar Nikolov <lazar.nikolov@sentry.io>
2025-01-19 16:21:30 -0500

    initial project

---
+++ app/routes/_layout.tsx
@@ -0,0 +1,13 @@
+import { Outlet, createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/_layout")({
+  component: LayoutComponent,
+});
+
+function LayoutComponent() {
+  return (
+    <div>
+      Hello "/_layout/"! <Outlet />
+    </div>
+  );
+}
